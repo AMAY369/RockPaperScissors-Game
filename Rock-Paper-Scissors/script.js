fefdff81654018f1e4032620a82dbfb35d0eeb3b
ABHAY GUPTA <105373537+AMAY369@users.noreply.github.com>
2023-10-19 20:04:28 +0530

    updated game

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,5 +1,6 @@
 let autoPlaying = false;
 let intervalID;
+
 document.querySelector('body').addEventListener('keydown',(event)=>{
   if(event.key==='a'){
     autoPlay();
@@ -17,21 +18,28 @@ document.querySelector('body').addEventListener('keydown',(event)=>{
     playGame('scissors');
   }
 });
+
 document.querySelector('.js-auto-play-button').addEventListener('click',()=>{
   autoPlay();
 });
+
 document.querySelector('.js-reset-button').addEventListener('click',()=>{
   resetScore();
 });
+
 document.querySelector('.js-scissors-button').addEventListener('click',()=>{
   playGame('scissors');
 });
+
 document.querySelector('.js-paper-button').addEventListener('click',()=>{
   playGame('paper');
 });
+
 document.querySelector('.js-rock-button').addEventListener('click',()=>{
   playGame('rock');
 });
+
+
 function autoPlay(){
   if(!autoPlaying){
     intervalID=setInterval(()=>{
@@ -50,19 +58,24 @@ function autoPlay(){
   else{
     document.querySelector('.js-auto-play-button').innerHTML='Auto Play';
   }
-}
+};
+
+
 function resetScore(){
       score.wins = 0;
       score.losses = 0;
       score.ties = 0;
       localStorage.removeItem('score');
       updateScoreElement();
-}
+};
+
+
 let score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
   losses: 0,
   ties: 0
 };
+
 updateScoreElement();
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
@@ -75,7 +88,7 @@ function playGame(playerMove) {
     } else if (computerMove === 'scissors') {
       result = 'Tie.';
     }
-  } 
+  }
   else if (playerMove === 'paper') {
     if (computerMove === 'rock') {
       result = 'You win.';
@@ -84,7 +97,7 @@ function playGame(playerMove) {
     } else if (computerMove === 'scissors') {
       result = 'You lose.';
     }
-  } 
+  }
   else if (playerMove === 'rock') {
     if (computerMove === 'rock') {
       result = 'Tie.';
@@ -104,27 +117,31 @@ function playGame(playerMove) {
   }
 
   localStorage.setItem('score', JSON.stringify(score));
-
   updateScoreElement();
-
   document.querySelector('.js-result').innerHTML = result;
+  document.querySelector('.js-player-move').innerHTML =
+  `You: <img src="images/${playerMove}-emoji.png">`;
 
-  document.querySelector('.js-player-move').innerHTML = `You: <img src="images/${playerMove}-emoji.png">`;
-  document.querySelector('.js-comp-move').innerHTML = `Computer: <img src='images/${computerMove}-emoji.png'>`;
+  document.querySelector('.js-comp-move').innerHTML=
+  `Computer: <img src='images/${computerMove}-emoji.png'>`;
 }
+
 
 function updateScoreElement() {
   document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
+
 
 function pickComputerMove() {
   const randomNumber = Math.random();
   let computerMove = '';
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = 'rock';
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+  }
+  else if(randomNumber >= 1/3 && randomNumber < 2/3){
     computerMove = 'paper';
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+  }
+  else if(randomNumber >= 2/3 && randomNumber < 1 ) {
     computerMove = 'scissors';
   }
   return computerMove;
